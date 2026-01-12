35d78ba61f7af15edc20bb5497e0e16c773d6441
matheusserafim23 <matheussud8@gmail.com>
2026-01-12 18:29:25 -0300

    add resume and job

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "software developer";
+        job1._startYear = 2014;
+        job1._endYear = 2022;
+
+        //job1.DisplayJobDetails();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Developer";
+        job2._company = "Google";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        //job2.DisplayJobDetails();
+
+
+        Resume myResume = new Resume();
+        myResume._name = "Matheus Serafim";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
---
+++ week02/Resumes/Resume.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+public class Resume
+{
+    public string _name;
+
+    public List<Job>_jobs = new List<Job>();
+
+    public void DisplayResume()
+    {
+        Console.WriteLine($"Name: {_name}");
+
+        foreach (Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+    
+}
