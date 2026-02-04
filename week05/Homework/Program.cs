0f9c4dfc5f2eed112bbddd530888573f401e4173
matheusserafim-git$ git config --global user.name <matheussud8@gmail.com>
2026-02-04 00:21:37 -0300

    homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Silvester", "fractions");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("lucia", "Math", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("marta Silva", "Brazilian History", "The live of Dom Pedro II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
+    
     }
 }
