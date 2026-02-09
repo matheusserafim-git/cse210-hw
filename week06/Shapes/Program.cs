53be2ff0860d50c8777519d4312ca6879007574c
matheusserafim-git$ git config --global user.name <matheussud8@gmail.com>
2026-02-09 18:55:23 -0300

    shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Drawing;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        Square s1 = new Square("Green", 3);
+        shapes.Add(s1);
+        
+        Circle c1 = new Circle("Blue", 4);
+        shapes.Add(c1);
+
+        Rectangle r1 = new Rectangle("Yellow", 5, 3);
+        shapes.Add(r1);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape: {shape.GetType().Name}, Color: {shape.GetColor()}, Area: {shape.GetArea()}");  
+        }
     }
 }
